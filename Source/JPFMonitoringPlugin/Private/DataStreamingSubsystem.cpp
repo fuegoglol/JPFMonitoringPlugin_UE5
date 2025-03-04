7f1315aaa182f8c0b58be477954bf8cbb3a50a41
fuegoglol <llou@etu.uqac.ca>
2025-03-04 16:54:29 -0500

    feat : auto close game after benchmark delay for automation

--- Source/JPFMonitoringPlugin/Private/DataStreamingSubsystem.cpp
+++ Source/JPFMonitoringPlugin/Private/DataStreamingSubsystem.cpp
@@ -1,13 +1,17 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DataStreamingSubsystem.h"
-
+#include "Misc/DateTime.h"
+#include "GenericPlatform/GenericPlatformMisc.h"
 #include "JPFMonitoringPlugin.h"
 #include "SerialCom.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 
 void UDataStreamingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
+
+	TickDelegateHandle = FTSTicker::GetCoreTicker().AddTicker(FTickerDelegate::CreateUObject(this, &UDataStreamingSubsystem::Tick));
 
 	Super::Initialize(Collection);
 
@@ -16,9 +20,14 @@ void UDataStreamingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	// COM PORT OPENING
 	bIsPortOpened = false;
 	SerialCom = USerialCom::OpenComPortWithFlowControl(bIsPortOpened, COM_PORT, BAUD_RATE);
+
 	if(!bIsPortOpened)
 	{
-		UE_LOG(LogTemp,Error,TEXT("Can't open port COM%d"),COM_PORT);
+
+		UE_LOG(LogTemp,Warning,TEXT("Can't open port COM%d"),COM_PORT);
+#if DO_CLOSE_GAME_AFTER_DELAY
+		UKismetSystemLibrary::QuitGame(GetWorld(),nullptr,EQuitPreference::Quit,true);
+#endif
 		return;
 	}
 
@@ -26,26 +35,36 @@ void UDataStreamingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 
 	// RESULTS FILE OPENING
 
-	FinalFileDestination = FString::Printf(TEXT("%s%hs%hs_%s.csv"),
+	
+	
+	FinalFileDestination = FString::Printf(TEXT("%s%hs%s/%hs_%s_%lli.csv"),
 		FPlatformProcess::UserDir(),
 		FILE_DESTINATION,
+		FPlatformProcess::ExecutableName(),
 		BASE_FILENAME,
-		*GetWorld()->GetMapName());
+		FPlatformProcess::ExecutableName(),
+		FDateTime::Now().GetTicks());
 
 }
 
 void UDataStreamingSubsystem::Deinitialize()
 {
+	FString Results;
+	
 	if(!bIsPortOpened || !SerialCom)
+	{
+		Results = "Error reading values";
+		
+		FFileHelper::SaveStringToFile(Results,*FinalFileDestination);
 		return;
+	}
+		
 
 	SerialCom->Close();
 
 
 	//Write results in file
 
-	FString Results;
-	
 
 	UE_LOG(LogTemp, Display,TEXT("Measures length : %i"),Measures.Num());
 	Results.Append(FMeasure::DisplayHeaderRow());
@@ -60,14 +79,13 @@ void UDataStreamingSubsystem::Deinitialize()
 	Super::Deinitialize();
 }
 
-void UDataStreamingSubsystem::Tick(float DeltaTime)
+bool UDataStreamingSubsystem::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
 
 	Time+=DeltaTime;
 	
 	if(!bIsPortOpened || !SerialCom)
-		return;
+		return true;
 	
 	bool bDidRead = false;
 	const FString ReadResults = SerialCom->Readln(bDidRead);
@@ -82,4 +100,18 @@ void UDataStreamingSubsystem::Tick(float DeltaTime)
 
 		Measures.Add(M);
 	}
+
+#if DO_CLOSE_GAME_AFTER_DELAY
+	if(Time >= BENCH_DELAY_IN_SECONDS)
+		
+#if UE_SERVER
+		FGenericPlatformMisc::RequestExit(false,TEXT("Killing process after desired bench delay"));
+#else
+		UKismetSystemLibrary::QuitGame(GetWorld(),nullptr,EQuitPreference::Type::Quit,true);
+#endif
+		
+#endif
+	
+	
+	return true;
 }
